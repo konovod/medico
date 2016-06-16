aa475600f1bcd662bed25e5c6f68496f6b7a0a05
Your Name <you@example.com>
2016-06-16 22:26:57 +0000

    test effector

--- spec/biology_spec.cr
+++ spec/biology_spec.cr
@@ -14,7 +14,22 @@ describe Biology do
   john = Patient.new("John")
 
   it "empty tick" do
-    john.process_tick
+    10.times{john.process_tick}
+    john.systems.values.first.params.get.values.first.real.should be_close(0.25, 0.01)
+  end
+
+  it "test effector" do
+    asys = ALL_SYSTEMS.first
+    aparam = ALL_PARAMS.first
+    testeff = ChangeParam.new(aparam, Fuzzy::Pike.new(f(-0.1)))
+    testdis = TimedEffector.new()
+    testdis.effects << testeff
+
+    john.systems[asys].effectors[testdis] = 10
+    5.times{john.process_tick}
+    john.systems.values.first.params.get.values.first.real.should be_close(0.15, 0.01)
+    10.times{john.process_tick}
+    john.systems.values.first.params.get.values.first.real.should be_close(0.25, 0.01)
   end
 
 
