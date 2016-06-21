1c8a6a6dd12f9da1bcc9a46a5598d75eefc00a32
Your Name <you@example.com>
2016-06-21 00:45:48 +0000

    class for testing not passed random

--- src/medico/globals.cr
+++ src/medico/globals.cr
@@ -11,4 +11,19 @@ def s(value)
   return $s[value]
 end
 
+class TEST_RND
+  include Random
+
+  def next_u32
+    raise "RANDOM NOT PASSED"
+  end
+  def initialize
+
+  end
+
+end
+
+
+DEF_RND = TEST_RND.new  #Random::DEFAULT
+
 # TODO: def fill_hash
