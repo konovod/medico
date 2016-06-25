7fb2a3bd09dc0ef7812a307d5884aabd310d64b9
Your Name <you@example.com>
2016-06-25 01:08:44 +0000

    preparing for name generator

---
+++ src/medico/data/names.cr
@@ -0,0 +1,61 @@
+
+CHEM_NAMES1 = {
+  { name: :AlchAdj1, value: 4},
+  { name: :AlchAdj2, value: 10},
+  { name: :AlchAdj3, value: 9},
+  { name: :AlchAdj4, value: 15},
+  { name: :AlchAdj5, value: 7},
+  { name: :AlchAdj6, value: 15},
+  { name: :AlchAdj7, value: 1},
+  { name: :AlchAdj8, value: 2},
+  { name: :AlchAdj9, value: 8},
+  { name: :AlchAdj10, value: 2},
+  { name: :AlchAdj11, value: 4},
+  { name: :AlchAdj12, value: 4},
+  { name: :AlchAdj13, value: 10},
+  { name: :AlchAdj14, value: 4},
+  { name: :AlchAdj15, value: 6},
+  { name: :AlchAdj16, value: 3},
+  { name: :AlchAdj17, value: 7},
+  { name: :AlchAdj18, value: 5},
+}
+
+  CHEM_NAMES2 = {
+  { name: :AlchNoun1, value: 5},
+  { name: :AlchNoun2, value: 5},
+  { name: :AlchNoun3, value: 5},
+  { name: :AlchNoun4, value: 15},
+  { name: :AlchNoun5, value: 2},
+  { name: :AlchNoun6, value: 8},
+  { name: :AlchNoun7, value: 2},
+  { name: :AlchNoun8, value: 3},
+  { name: :AlchNoun9, value: 5},
+  { name: :AlchNoun12, value: 10},
+  { name: :AlchNoun13, value: 4},
+  { name: :AlchNoun14, value: 3},
+}
+
+
+DIS_NAMES2 = {
+{ name: :Disease1, value: 5},
+{ name: :Disease2, value: 6},
+{ name: :Disease3, value: 10},
+{ name: :Disease4, value: 1},
+{ name: :Disease5, value: 5},
+{ name: :Disease6, value: 8},
+{ name: :Disease7, value: 3},
+{ name: :Disease8, value: 3},
+{ name: :Disease9, value: 7},
+}
+
+DIS_NAMES1 = {
+{ name: :AdjPart1, value: 10},
+{ name: :AdjPart2, value: 8},
+{ name: :AdjPart3, value: 6},
+{ name: :AdjPart4, value: 4},
+{ name: :AdjPart5, value: 8},
+{ name: :AdjPart6, value: 5},
+{ name: :AdjPart7, value: 12},
+{ name: :AdjPart8, value: 4},
+{ name: :AdjPart9, value: 4},
+}
