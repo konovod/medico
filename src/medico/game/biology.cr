fa49804d0038750bebc7f4024cb3f2972fd7524e
Konovod Andrey <konovod@gmail.com>
2018-05-18 11:02:18 +0300

    fixed rng name

--- src/medico/game/biology.cr
+++ src/medico/game/biology.cr
@@ -275,7 +275,7 @@ module Biology
   abstract class Effector
     getter effects
 
-    alias Context = NamedTuple(state: SystemState, data: TEffectorData, random: Random::MT19937)
+    alias Context = NamedTuple(state: SystemState, data: TEffectorData, random: Random::PCG32)
 
     abstract def process(**context) : TEffectorData
 
